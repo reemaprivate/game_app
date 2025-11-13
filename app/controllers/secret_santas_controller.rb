d31733791c67d7c900eca2548d58af143d5aa3d5
reemaprivate <naikreema535@gmail.com>
2025-11-14 01:03:52 +0530

    fixed some annotations issues

--- app/controllers/secret_santas_controller.rb
+++ app/controllers/secret_santas_controller.rb
@@ -1,25 +1,23 @@
 require "csv"
 
 class SecretSantasController < ApplicationController
-
   def index
-
   end
 
   def create
     begin
-      #check for uploaded files
+      # check for uploaded files
       if params[:employee_csv].blank? || params[:prev_csv].blank?
         flash[:alert] = "Both the CSV files are required!"
         render :index and return
       end
 
       CsvImportService.import_file params[:employee_csv].path
-      CsvImportService.import_past_data params[:prev_csv].path,Date.today.prev_year.year
-      @data = SecretSantaSelectionService.new().roulette
-      output_path = Rails.root.join('tmp', 'secret_santa_result.csv')
-      CsvExportService.export_file(@data,output_path)
-      flash[:notice] = "Secret Santa result file has been successfully generated!" if File.exist?output_path
+      CsvImportService.import_past_data params[:prev_csv].path, Date.today.prev_year.year
+      @data = SecretSantaSelectionService.new.roulette
+      output_path = Rails.root.join("tmp","secret_santa_result.csv")
+      CsvExportService.export_file( @data, output_path)
+      flash[:notice] = "Secret Santa result file has been successfully generated!" if File.exist? output_path
       send_file output_path, filename: "secret_santa_result.csv"
     rescue CsvImportService::FileMissingError, CsvImportService::IncorrectHeaderError, CsvImportService::InvalidData => e
       flash[:alert] = "CSV Import Error: #{e.message}"
