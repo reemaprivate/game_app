ac180c2e445a3a67819f4211cb9f01680f74c30d
reemaprivate <naikreema535@gmail.com>
2025-11-13 20:33:59 +0530

    added validation to create unique association per year

--- app/models/secret_santa_association.rb
+++ app/models/secret_santa_association.rb
@@ -3,4 +3,9 @@ class SecretSantaAssociation < ApplicationRecord
   belongs_to :giver , :class_name => "Employee"
   belongs_to :recipient, :class_name => "Employee"
 
+  validates :giver_id,:recipient_id,:year , :presence => true
+  validates :giver_id, :uniqueness => {:scope =>  :year ,message: "is already assigned for a recipient this year!" }
+  validates :recipient_id, :uniqueness => {:scope => :year ,message: "is already assigned to a giver this year!" }
+
+
 end
