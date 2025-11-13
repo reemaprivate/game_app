8aefcb5c715898276a08ca12521ac532146553cb
reemaprivate <naikreema535@gmail.com>
2025-11-13 23:36:00 +0530

    updated logic for roulette when past data is present and added scopes in association model

--- app/models/secret_santa_association.rb
+++ app/models/secret_santa_association.rb
@@ -7,5 +7,8 @@ class SecretSantaAssociation < ApplicationRecord
   validates :giver_id, :uniqueness => {:scope =>  :year ,message: "is already assigned for a recipient this year!" }
   validates :recipient_id, :uniqueness => {:scope => :year ,message: "is already assigned to a giver this year!" }
 
-
+  scope :past_year , -> { where(:year => Date.today.prev_year.year)}
+  scope :this_year, -> { where(:year => Date.today.year)}
+  scope :for_giver, -> (giver_id){ where(:giver_id => giver_id)}
+  scope :for_recipient, -> (recipient_id){ where(:recipient_id => recipient_id)}
 end
--- app/services/secret_santa_selection_service.rb
+++ app/services/secret_santa_selection_service.rb
@@ -4,19 +4,49 @@ class SecretSantaSelectionService
     @employees = Employee.all
   end
 
-  def roulette
-    #initital logic to create test data
-    first = @employees.first
-    last = @employees.last
-    data = []
-    @employees.each_with_index  do |emp,index|
-      @employees[index+1]
-      if emp == last
-        data << {:giver => emp, :recipient => first}
-      else
-        data << {:giver => emp, :recipient => @employees[index+1]}
+  def assign_secret_santa(givers, available_recipients, past_year_pairs = {})
+    return [] if givers.empty?
+
+    giver = givers.first
+
+    available_recipients.each do |child|
+      next if child == giver
+      next if past_year_pairs[giver.id]&.include?(child.id)
+
+      remaining_givers = givers[1..-1]
+      remaining_children = available_recipients - [child]
+
+      result = assign_secret_santa(remaining_givers, remaining_children, past_year_pairs)
+      if result
+        return [{ giver: giver, recipient: child }] + result
       end
     end
+    return nil
+  end
+
+  def roulette
+    employees = Employee.all.to_a
+    data = []
+    past_year_pairs = {}
+    @employees.each do |emp|
+      past_year_pairs[emp.id] = emp.giver_associations.past_year.pluck(:recipient_id)
+    end
+
+    assignments = assign_secret_santa(employees, employees, past_year_pairs)
+
+    if assignments
+      assignments.each do |pair|
+        SecretSantaAssociation.create!(
+          giver: pair[:giver],
+          recipient: pair[:recipient],
+          year: Date.today.year
+        )
+        data << { :giver => pair[:giver], :recipient => pair[:recipient] }
+      end
+    else
+      puts "-invalid data -"
+    end
+
     data
   end
 
